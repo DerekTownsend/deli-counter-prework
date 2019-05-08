1f72047fe763725d21a5f44617f662bc2b3045b2
Derek Townsend <derekjavon2@gmail.com>
2019-05-08 04:09:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
   else
     line="The line is currently: "
     array.each.with_index do |person,position|
-      line+="#{position+1}. #{person}"
+      line+=" #{position+1}. #{person}"
     end
     puts line
   end
