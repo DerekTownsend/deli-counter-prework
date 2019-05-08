8e7ef291a3780b10f5d32a3398aa29c93f22c922
Derek Townsend <derekjavon2@gmail.com>
2019-05-08 04:15:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,6 +15,6 @@ end
 
 def take_a_number(array,name)
   array.push(name)
-  puts "Welcome, Ada. You are number 1 in line."
+  puts "Welcome, #{name}. You are number #{array.length} in line."
   return array
 end
