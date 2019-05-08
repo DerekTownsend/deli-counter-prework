5d1ec15290ea6c9e07154d4c3b5f540c062eea0e
Derek Townsend <derekjavon2@gmail.com>
2019-05-08 04:08:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,9 +5,8 @@ def line(array)
   if array.length==0
     puts "The line is currently empty."
   else
-    line="The line is currently: "
     array.each.with_index do |person,position|
-      line+"#{position+1}. #{person}"
+      line+"The line is currently: #{position+1}. #{person}"
     end
     puts line
   end
