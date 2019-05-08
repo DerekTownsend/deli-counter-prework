ca3b076c3849316be026d5e01d40b556bb984df0
Derek Townsend <derekjavon2@gmail.com>
2019-05-08 03:58:40 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,11 @@
+# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length==0
+    puts "The line is currently empty."
+    else
+  array.each.with_index do |person,position|
+
+  end
+end
